d1800765f5bbbcaabdd17bbefb30aff112863f77
Khushi Singh <khushiisingh100@gmail.com>
2023-09-09 16:21:05 +0530

    Add files via upload

---
+++ C++ Basics/TakingInput.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout << "Enter a number: " << endl;
+    int number;
+
+    //taking input from user
+    cin >> number;
+    cout << "You entered number: " << number << endl;
+}
