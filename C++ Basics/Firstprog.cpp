8ca359e79c32f105549a88bcae4c460e716c53dd
Khushi Singh <khushiisingh100@gmail.com>
2023-09-09 14:51:51 +0530

    Add files via upload

---
+++ C++ Basics/Firstprog.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout << "Hello World!\n";                         /*  '/n' adds new line;
+                                                        new line can also be added using ---> cout << endl; OR
+                                                        added using ---> cout << "Hello World" << endl;   */         
+    cout << "Welcome to C++ programming...";
+}
