784090108a64086dd5d97db47505b961760b1ec4
Khushi Singh <khushiisingh100@gmail.com>
2023-09-09 22:53:02 +0530

    Add files via upload

---
+++ C++ Basics/Variables.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    long long a=10;
+    cout << "Value of a: " << a << endl;
+    cout << sizeof(a) << endl;                //returns space of a datatype occupied in a memory location  
+   
+
+    char ch = 'a';
+    cout << "Value of ch : " << ch << endl;
+} 
