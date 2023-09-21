d3f0ff6b489e9bc049dc4f71828cdffad675c004
Khushi Singh <khushiisingh100@gmail.com>
2023-09-22 03:56:59 +0530

    Add files via upload

---
+++ Arrays/TwoPairSum.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int> arr{10,20,40,60,70};
+    int sum=80;
+    //print all pairs
+    //outer loop will traverse for each element
+    for(int i=0; i<arr.size();i++){
+        int element=arr[i];
+
+        //outer loop will traverse next upcoming elements
+        for(int j=i+1; j<arr.size();j++){
+           if(element+arr[j]==sum){
+            cout<<"Pair Found "<<element<<","<<arr[j]<<endl;
+           }
+        }
+    }
+}
